6f3cb40ae03ce3d3f1519f67ea6e7e14c4d057a6
Einelus003 <emmanuelinelus@gmail.com>
2025-10-14 18:44:02 -0400

    Exercice tracke assignment

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciceTracker
 {
-    static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        public static void Main()
+        {
+            List<Activity> activities = new List<Activity>
+            {
+                new Running(new DateTime(2025, 11, 3), 30, 4.8),
+                new Cycling(new DateTime(2025, 11, 3), 45, 20.0),
+                new Swimming(new DateTime(2025, 11, 3), 30, 40)
+            };
+
+            foreach (var activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+        }
     }
 }
